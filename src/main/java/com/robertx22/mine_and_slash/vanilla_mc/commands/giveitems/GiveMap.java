95824d5979da825c562a69ab937dc35ccef9e74c
SaloEater <danil.lazarev@gmail.com>
2025-08-27 14:07:47 -0500

    fix summon spawn, mob stuck in walls fixes

--- src/main/java/com/robertx22/mine_and_slash/vanilla_mc/commands/giveitems/GiveMap.java
+++ src/main/java/com/robertx22/mine_and_slash/vanilla_mc/commands/giveitems/GiveMap.java
@@ -4,6 +4,8 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.robertx22.dungeon_realm.item.DungeonItemMapData;
+import com.robertx22.dungeon_realm.item.DungeonItemNbt;
 import com.robertx22.mine_and_slash.database.registry.ExileDB;
 import com.robertx22.mine_and_slash.loot.LootInfo;
 import com.robertx22.mine_and_slash.loot.blueprints.MapBlueprint;
@@ -11,6 +13,7 @@ import com.robertx22.mine_and_slash.uncommon.datasaving.StackSaving;
 import com.robertx22.mine_and_slash.uncommon.utilityclasses.PlayerUtils;
 import com.robertx22.mine_and_slash.vanilla_mc.commands.CommandRefs;
 import com.robertx22.mine_and_slash.vanilla_mc.commands.suggestions.GearRaritySuggestions;
+import com.robertx22.mine_and_slash.vanilla_mc.commands.suggestions.MapTypeSuggestions;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.world.entity.player.Player;
@@ -28,17 +31,18 @@ public class GiveMap {
                 literal(CommandRefs.ID)
                         .then(literal("give").requires(e -> e.hasPermission(2))
                                 .then(literal("map")
-                                        .then(argument("target", EntityArgument.player())
-                                                .then(argument("level", IntegerArgumentType.integer()).then(argument(
-                                                        "rarity", StringArgumentType.string()).suggests(new GearRaritySuggestions())
-                                                        .then(argument("amount", IntegerArgumentType.integer(1, 5000)).executes(e -> execute(
-                                                                e.getSource(), EntityArgument.getPlayer(e, "target"), IntegerArgumentType.getInteger(e, "level"), StringArgumentType.getString(e, "rarity"),
-                                                                IntegerArgumentType.getInteger(e, "amount")
-                                                        )))))))));
+                                        .then(argument("type", StringArgumentType.string()).suggests(new MapTypeSuggestions())
+                                            .then(argument("target", EntityArgument.player())
+                                                    .then(argument("level", IntegerArgumentType.integer()).then(argument(
+                                                            "rarity", StringArgumentType.string()).suggests(new GearRaritySuggestions())
+                                                            .then(argument("amount", IntegerArgumentType.integer(1, 5000)).executes(e -> execute(
+                                                                    e.getSource(), EntityArgument.getPlayer(e, "target"), IntegerArgumentType.getInteger(e, "level"), StringArgumentType.getString(e, "rarity"),
+                                                                    IntegerArgumentType.getInteger(e, "amount"), StringArgumentType.getString(e, "type")
+                                                            ))))))))));
     }
 
     private static int execute(CommandSourceStack commandSource, Player player, int lvl,
-                               String rarity, int amount) {
+                               String rarity, int amount, String type) {
 
 
         if (Objects.isNull(player)) {
@@ -63,6 +67,13 @@ public class GiveMap {
             }
 
             StackSaving.MAP.saveTo(mapStack, b.createData());
+
+            if (!type.equals("random")) {
+                DungeonItemMapData map = DungeonItemNbt.DUNGEON_MAP.loadFrom(mapStack);
+                map.forced_dungeon_id = type;
+                DungeonItemNbt.DUNGEON_MAP.saveTo(mapStack, map);
+            }
+
             PlayerUtils.giveItem(mapStack, player);
         }
 

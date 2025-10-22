35ebf7f97ee40d01fdcfad2b18c1c2157a0af304
SaloEater <danil.lazarev@gmail.com>
2025-10-22 10:34:23 -0500

    Make master bag in curio slot to open other tab

--- src/main/java/com/robertx22/mine_and_slash/vanilla_mc/packets/backpack/OpenBackpackPacket.java
+++ src/main/java/com/robertx22/mine_and_slash/vanilla_mc/packets/backpack/OpenBackpackPacket.java
@@ -2,12 +2,16 @@ package com.robertx22.mine_and_slash.vanilla_mc.packets.backpack;
 
 import com.robertx22.library_of_exile.main.MyPacket;
 import com.robertx22.library_of_exile.packets.ExilePacketContext;
+import com.robertx22.mine_and_slash.a_libraries.curios.CuriosSlots;
+import com.robertx22.mine_and_slash.a_libraries.curios.MyCuriosUtils;
 import com.robertx22.mine_and_slash.capability.player.BackpackItem;
 import com.robertx22.mine_and_slash.capability.player.data.Backpacks;
 import com.robertx22.mine_and_slash.mmorpg.SlashRef;
 import com.robertx22.mine_and_slash.uncommon.datasaving.Load;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import org.jetbrains.annotations.Nullable;
 
 public class OpenBackpackPacket extends MyPacket<OpenBackpackPacket> {
 
@@ -36,11 +40,26 @@ public class OpenBackpackPacket extends MyPacket<OpenBackpackPacket> {
 
     @Override
     public void onReceived(ExilePacketContext ctx) {
-        int rows = 0;
-        if (ctx.getPlayer().getMainHandItem().getItem() instanceof BackpackItem i) {
-            rows = i.getSlots() / 9;
-            Load.backpacks(ctx.getPlayer()).getBackpacks().openBackpack(type, ctx.getPlayer(), rows);
+        Player player = ctx.getPlayer();
+        BackpackItem backpack = getBackpackItem(player);
+
+        if (backpack != null) {
+            int rows = backpack.getSlots() / 9;
+            Load.backpacks(player).getBackpacks().openBackpack(type, player, rows);
         }
+    }
+
+    private static @Nullable BackpackItem getBackpackItem(Player player) {
+        if (player.getMainHandItem().getItem() instanceof BackpackItem mainHand) {
+            return mainHand;
+        }
+
+        var curioBackpack = MyCuriosUtils.get(CuriosSlots.MASTER_BAG.name, player, 0);
+        if (!curioBackpack.isEmpty() && curioBackpack.getItem() instanceof BackpackItem curioBackpackItem) {
+            return curioBackpackItem;
+        }
+
+        return null;
     }
 
     @Override

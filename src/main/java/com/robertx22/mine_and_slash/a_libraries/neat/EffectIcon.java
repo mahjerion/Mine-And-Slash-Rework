e27e6b94dc3e5fede3844f86f317d205bdf3c4ad
AltimorTASDK <altimormc@gmail.com>
2025-10-22 23:11:29 -0400

    Fix health bar shader compatibility Use POSITION_COLOR_TEX_LIGHTMAP vertex format like before so Iris considers it a font glyph

--- src/main/java/com/robertx22/mine_and_slash/a_libraries/neat/EffectIcon.java
+++ src/main/java/com/robertx22/mine_and_slash/a_libraries/neat/EffectIcon.java
@@ -21,14 +21,15 @@ public record EffectIcon(ResourceLocation location, int stack) {
     public void renderOnHealthBar(PoseStack poseStack, MultiBufferSource source, int size){
         VertexConsumer buffer = source.getBuffer(NeatRenderType.getHealthBarIconType(location));
         Matrix4f pose = new Matrix4f(poseStack.last().pose());
-        int alpha = 200;
-        int i = 255;
+        final int light = 0xF000F0;
+        final int alpha = 200;
+        final int i = 255;
         Font font = Minecraft.getInstance().font;
         float fontScale = 0.5f * size / font.lineHeight;
-        buffer.vertex(pose, 0, 0, 0.01f).color(i, i , i, alpha).uv(0, 0).endVertex();
-        buffer.vertex(pose, 0, size, 0.01f).color(i, i , i, alpha).uv(0, 1).endVertex();
-        buffer.vertex(pose, size, size, 0.01f).color(i, i , i, alpha).uv(1, 1).endVertex();
-        buffer.vertex(pose, size, 0, 0.01f).color(i, i , i, alpha).uv(1, 0).endVertex();
+        buffer.vertex(pose, 0, 0, 0.01f).color(i, i , i, alpha).uv(0, 0).uv2(light).endVertex();
+        buffer.vertex(pose, 0, size, 0.01f).color(i, i , i, alpha).uv(0, 1).uv2(light).endVertex();
+        buffer.vertex(pose, size, size, 0.01f).color(i, i , i, alpha).uv(1, 1).uv2(light).endVertex();
+        buffer.vertex(pose, size, 0, 0.01f).color(i, i , i, alpha).uv(1, 0).uv2(light).endVertex();
         poseStack.pushPose();
 
         //poseStack.translate(size - font.width(stack + ""), size - font.lineHeight, 0);

27e7736ed3fae8ab8c22c1e56d5d032543152dd0
AltimorTASDK <altimormc@gmail.com>
2025-10-25 19:19:39 -0400

    fix player inv being shifted up one pixel on the backpack menu

--- src/main/java/com/robertx22/mine_and_slash/capability/player/container/BackpackMenu.java
+++ src/main/java/com/robertx22/mine_and_slash/capability/player/container/BackpackMenu.java
@@ -44,12 +44,12 @@ public class BackpackMenu extends AbstractContainerMenu {
             }
             for (int l = 0; l < 3; ++l) {
                 for (int j1 = 0; j1 < 9; ++j1) {
-                    this.addSlot(new Slot(playerINV, j1 + l * 9 + 9, 8 + j1 * 18, 103 + l * 18 + i));
+                    this.addSlot(new Slot(playerINV, j1 + l * 9 + 9, 8 + j1 * 18, 104 + l * 18 + i));
                 }
             }
 
             for (int i1 = 0; i1 < 9; ++i1) {
-                this.addSlot(new Slot(playerINV, i1, 8 + i1 * 18, 161 + i));
+                this.addSlot(new Slot(playerINV, i1, 8 + i1 * 18, 162 + i));
             }
         } catch (Exception e) {
             e.printStackTrace();
